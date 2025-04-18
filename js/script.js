40639f0153ee5f56f0fb47bef3e5710b4d8ecf46
shrey688 <shreyawankhede873@gmail.com>
2025-04-18 19:15:07 +0530

    changed

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hero = document.querySelector('.hero');
+    const images = [
+      'images/back.jpg',
+      'images/bg1.png',
+      'images/bg2.png'
+    ];
+    let index = 0;
+  
+    setInterval(() => {
+      index = (index + 1) % images.length;
+      hero.style.backgroundImage = `url('${images[index]}')`;
+    }, 10000); // 10 seconds
+  });
+  
+  document.getElementById("toggleButton").addEventListener("click", function () {
+    const menu = document.getElementById("categoryMenu");
+    menu.classList.toggle("d-none");
+  });
+
+  
+  
